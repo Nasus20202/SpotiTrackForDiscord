567db16fcdb9145220659c5a5f890596d2b564c4
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2022-01-03 16:21:42 +0100

    Check if song is over functionality

---
+++ main.py
@@ -0,0 +1,53 @@
+from asyncio.tasks import current_task
+from dotenv import load_dotenv
+import spotify
+import signal
+import sys
+import asyncio
+import nest_asyncio
+import database
+
+
+def handler(signum, frame):
+    res = input("Ctrl-c was pressed. Do you really want to exit? y/n ")
+    if res == 'y':
+        sys.exit(0)
+ 
+signal.signal(signal.SIGINT, handler)
+
+load_dotenv()
+nest_asyncio.apply()
+
+db = database.create_connection("songs.db")
+
+async def checkIfSongIsOver(id):
+    progress = spotify.get_milliseconds()
+    duration = spotify.get_duration_milliseconds()
+    current_id = spotify.get_track_id()
+    if(progress < 5000):
+        id = ""
+    if(duration - progress < 5000 and current_id != id):
+        print(spotify.get_current_track())
+        await asyncio.sleep(1)
+        return current_id
+    await asyncio.sleep(1)
+    return id
+
+
+async def thread():
+    i = 0
+    id = ""
+    while True:
+        id = await checkIfSongIsOver(id)
+        if(i == 1000):
+            spotify.refresh_access_token()
+            i = 0
+        i = i + 1
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+task = loop.create_task(thread())
+#try:
+#    loop.run_until_complete(task)
+#except asyncio.CancelledError:
+#    pass
---
+++ spotify.py
@@ -0,0 +1,111 @@
+from asyncio.tasks import sleep
+import asyncio
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def refresh_access_token():
+    refresh_token = os.environ["SPOTIFY_REFRESH_TOKEN"]
+    params = (
+        ('refresh_token', refresh_token),
+    )
+    try:
+        response = requests.get(os.environ["TOKEN_GENERATOR_SERVER"] + '?refresh_token=' + refresh_token)
+        os.environ["SPOTIFY"] = response.json()["access_token"]
+        print("New token generated")
+    except:
+        print("Cannot refresh the token!")
+        asyncio.sleep(1)
+        refresh_access_token()
+
+refresh_access_token()
+
+def __milis_to_time__(miliseconds):
+    minutes = miliseconds // 60000
+    seconds = (miliseconds - minutes * 60000) // 1000
+    time = ""
+    if(minutes < 10):
+        time = time + '0'
+    time = time + str(minutes) + ":"
+    if(seconds < 10):
+        time = time + '0'
+    time = time + str(seconds)
+    return time
+
+def get_current_spotify_info():
+    try:
+        response = requests.get("https://api.spotify.com/v1/me/player/currently-playing?market=PL", headers= {"Authorization" : "Bearer " + os.environ["SPOTIFY"]})
+    except:
+        print("Cannot get data from Spotify API")
+        return [404]
+    if(response.status_code != 200):
+        return [response.status_code, {}]
+    return [response.status_code, response.json()]
+
+def get_current_artists():
+    data = get_current_spotify_info()
+    if(data[0]!=200):
+        return
+    artists = ""
+    first = True
+    for artist in data[1]["item"]["artists"]:
+        if(not first):
+            artists = artists + ", "
+        first = False
+        artists = artists + artist["name"]
+    return artists
+
+def get_current_track():
+    data = get_current_spotify_info()
+    if(data[0]!=200):
+        return
+    return data[1]["item"]["name"]
+
+def get_album_covers():
+    data = get_current_spotify_info()
+    if(data[0]!=200):
+        return
+    images = []
+    for image in data[1]["item"]["album"]["images"]:
+        images.append(image["url"])
+    return images
+
+def get_milliseconds():
+    data = get_current_spotify_info()
+    if(data[0]!=200):
+        return 0
+    else:
+        return data[1]["progress_ms"]
+
+def get_duration_milliseconds():
+    data = get_current_spotify_info()
+    if(data[0]!=200):
+        return 0
+    else:
+        return data[1]["item"]["duration_ms"]
+
+def get_progress():
+    data = get_current_spotify_info()
+    if(data[0]!=200):
+        return
+    return __milis_to_time__(data[1]["progress_ms"])
+
+def get_duration():
+    data = get_current_spotify_info()
+    if(data[0]!=200):
+        return
+    return __milis_to_time__(data[1]["item"]["duration_ms"])
+    
+
+def get_response_code():
+    data = get_current_spotify_info()
+    return data[0]
+
+def get_track_id():
+    data = get_current_spotify_info()
+    if(data[0]!=200):
+        return
+    return data[1]["item"]["id"]
