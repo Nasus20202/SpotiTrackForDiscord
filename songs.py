a0a4ed6f4a1ef95c057b6ed2ebb84737afb1296a
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2022-02-28 22:02:45 +0100

    Total plays added

--- songs.py
+++ songs.py
@@ -4,9 +4,10 @@ import database
 import time_manager
 
 def stats(n):
-    bio = "Time spent listening: " + time_manager.get_current_time_spent() + " (d:h:m:s)\n"
+    bio = f"Time spent listening: {time_manager.get_current_time_spent()} (d:h:m:s)\n"
     songs = database.get_top_song(n)
     i = 1
+    totalPlays = 0
     artistsPopularity = {}
     for song in songs:
         name = spotify.get_track_name_by_id(song[0])
@@ -18,12 +19,14 @@ def stats(n):
             if artist not in artistsPopularity:
                 artistsPopularity[artist] = 0
             artistsPopularity[artist] += plays
+        totalPlays += plays
     bio += f"\nArtists popularity in your top {n} tracks:\n"
     i = 1
     for artists in dict(sorted(artistsPopularity.items(), key=lambda item: item[1], reverse=True)):
         bio += f"#{i} {artists} {artistsPopularity[artists]}\n"
         i+=1
     sleep(0.05) # just for API rate limit
+    bio += f'Total plays: {totalPlays} - time spent: {time_manager.get_current_time_spent()}'
     return bio
 
 print(stats(int(input("How many songs would you like to see? "))))
