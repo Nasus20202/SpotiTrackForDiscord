e92c505313f160146f18e2c20ecd02c13fcf6d2a
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2022-02-28 21:53:03 +0100

    Artists stats avaiable in songs.py scripts

--- discord_bio.py
+++ discord_bio.py
@@ -1,18 +1,26 @@
 import requests
 from dotenv import load_dotenv
+import unicodedata
 import os
 
 load_dotenv()
 
 
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
+                  
 def set(bio):
     headers = {
         'Content-Type': 'application/json',
         'Authorization': os.environ["TOKEN"],
     }
-    data = '{"bio":"' + str(bio[:190]) + '"\n}'
+    data = '{"bio":"' + strip_accents(str(bio[:190])) + '"\n}'
     try:
+        print(headers)
+        print(data)
         response = requests.patch('https://discord.com/api/v9/users/@me', headers=headers, data=data)
+        print(response.content)
         return response.status_code
     except:
         print("Cannot change Discord Bio")
---
+++ songs.py
@@ -0,0 +1,29 @@
+from time import sleep
+import spotify
+import database
+import time_manager
+
+def stats(n):
+    bio = "Time spent listening: " + time_manager.get_current_time_spent() + " (d:h:m:s)\n"
+    songs = database.get_top_song(n)
+    i = 1
+    artistsPopularity = {}
+    for song in songs:
+        name = spotify.get_track_name_by_id(song[0])
+        plays = song[1]
+        artists = spotify.get_track_artist(song[0])
+        bio += f'#{str(i)} {name} by {artists} ({str(plays)})\n'
+        i += 1
+        for artist in artists.split(', '):
+            if artist not in artistsPopularity:
+                artistsPopularity[artist] = 0
+            artistsPopularity[artist] += plays
+    bio += f"\nArtists popularity in your top {n} tracks:\n"
+    i = 1
+    for artists in dict(sorted(artistsPopularity.items(), key=lambda item: item[1], reverse=True)):
+        bio += f"#{i} {artists} {artistsPopularity[artists]}\n"
+        i+=1
+    sleep(0.05) # just for API rate limit
+    return bio
+
+print(stats(int(input("How many songs would you like to see? "))))
