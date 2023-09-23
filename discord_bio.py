878ec5d984b3f56c353619c9218ba2be05cc2edc
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-09-23 22:02:59 +0200

    fix: change profile endpoint

--- discord_bio.py
+++ discord_bio.py
@@ -17,7 +17,7 @@ def set(bio):
     }
     data = '{"bio":"' + strip_accents(str(bio[:190])) + '"\n}'
     try:
-        response = requests.patch('https://discord.com/api/v9/users/@me', headers=headers, data=data, timeout=3)
+        response = requests.patch('https://discord.com/api/v9/users/@me/profile', headers=headers, data=data, timeout=3)
         return response.status_code
     except:
         print("Cannot change Discord Bio")
